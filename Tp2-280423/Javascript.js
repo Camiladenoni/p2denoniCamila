e55ae58f80c50f95c9930af44e5b77e2b89e9037
Camiladenoni <129554141+Camiladenoni@users.noreply.github.com>
2023-05-05 15:19:27 -0300

    Add files via upload

---
+++ Tp2-280423/Javascript.js
@@ -0,0 +1,6 @@
+function getCharacter () {
+    fetch(https://rickandmortyapi.com/api)
+    .then (function(response){
+    return response.json()})
+    .console.log(name.status.species.ciudad.foto)
+}
